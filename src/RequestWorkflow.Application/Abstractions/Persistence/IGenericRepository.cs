5674fe91171300a7038d9e81b259d23aaaeebde2
GhandchiMorteza <mortezaghandchi7@gmail.com>
2026-09-12 22:53:05 +0330

    feat(application): add persistence abstractions

---
+++ src/RequestWorkflow.Application/Abstractions/Persistence/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace RequestWorkflow.Application.Abstractions.Persistence;
+
+public interface IGenericRepository<TEntity>
+    where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    Task<TEntity?> FirstOrDefaultAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> ListAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(
+        TEntity entity,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/RequestWorkflow.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace RequestWorkflow.Application.Abstractions.Persistence;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default);
+}
